f524f2b22e2fce157269f3956a0a68c18bfd37e7
Vinay <vsingh@grepruby.com>
2017-07-12 16:17:24 +0530

    Update mock property test

--- __tests__/Intro.test.js
+++ __tests__/Intro.test.js
@@ -265,14 +265,172 @@ import Intro from '../Intro';
 // });
 
 // /* function  mocking */
-// it('Test function mocking', () => {
-//   const myMock = jest.fn();
-//   const a = new myMock();
-//   const b = {};
-//   const bound = myMock.bind(b);
-//   bound();
-//   console.log('Module mocking', moduleName());
-// });
+describe('Testing function  mocking', () => {
+
+  // it('Basic mock function testing', () => {
+  //   const myMock = jest.fn();
+  //   console.log('Basic mock function testing 1 => ', myMock());
+  //   console.log('Basic mock function testing 2 => ', myMock.mock);
+  //   console.log('Basic mock function testing 3 => ', myMock.mock.calls);
+  //   console.log('Basic mock function testing 4 => ', myMock.mock.instances);
+  // });
+
+  // it('myMock.mock.calls testing', () => {
+  //   console.log('myMock.mock.calls testing 0 => ', myMock);
+  //   // const myMock = jest.fn();
+  //   const myMock = jest.fn((name) => {
+  //      return `hello ${name}`;
+  //   });
+  //   // console.log('myMock.mock.calls testing 1 => ', myMock);
+  //   myMock('Me'); // call 1
+  //   // console.log('myMock.mock.calls testing 1.0 => ', myMock('Me'));
+  //   myMock('you'); // call 2
+  //   myMock(); // call 3
+  //   myMock(); // call 4
+  //   myMock(); // call 5
+  //   console.log('myMock.mock.calls testing 1.1 => ', myMock.mock);
+  //   console.log('myMock.mock.calls testing 1.2 => ', myMock.mock.calls);
+  //   console.log('myMock.mock.calls testing 1.3 => ', myMock.mock.calls[0]);
+  //   myMock('a', 'b');
+  //   console.log('myMock.mock.calls testing 3 => ', myMock.mock);
+  //   console.log('myMock.mock.calls testing 3.1 => ', myMock.mock.calls);
+  //   console.log('myMock.mock.calls testing 3.2 => ', myMock.mock.calls.length);
+  // });
+
+  // it('myMock.mock.instances testing', () => {
+  //   const myMock = jest.fn((name) => {
+  //     return `hello ${name}`;
+  //   });
+  //   const a = new myMock();
+  //   const b = new myMock();
+  //   const c = new myMock('Vinay');
+  //   console.log('myMock.mock.instances testing 1 => ', myMock.mock);
+  //   console.log('myMock.mock.instances testing 1.1 => ', myMock.mock.instances);
+  //   console.log('myMock.mock.instances testing 1.2 => ', myMock.mock.instances[0] === a);
+  //   console.log('myMock.mock.instances testing 1.3 => ', myMock.mock.instances[0] === b);
+  //   console.log('myMock.mock.instances testing 1.4 => ', c);
+  // });
+
+  // it('mockFn.mockClear() testing', () => {
+  //   const myMock = jest.fn();
+  //   myMock('1');
+  //   myMock('a', 'b');
+  //   const a = new myMock('Me');
+  //   const b = new myMock('You');
+  //   const c = new myMock();
+  //   const d = new myMock();
+  //   console.log('mockFn.mockClear() testing before => ', myMock.mock);
+  //   myMock.mockClear();
+  //   console.log('mockFn.mockClear() testing after => ', myMock.mock);
+  // });
+
+  // it('mockFn.mockImplementation(fn) testing', () => {
+  //   const myMock = jest.fn().mockImplementation(name => `hello ${name}`);
+  //   // or: const myMock = jest.fn((name) =>
+  //   //   `hello ${name}`
+  //   // );
+  //   // const a = new myMock('Vinay');
+  //   const a = myMock('Vinay');
+  //   console.log('mockFn.mockImplementation(fn) testing 1 => ', a === 'Vinay'); // false
+  //   console.log('mockFn.mockImplementation(fn) testing 2 => ', myMock.mock.calls[0]); // [ 'Vinay' ]
+  //   console.log('mockFn.mockImplementation(fn) testing 3 => ', myMock.mock.calls[0][0]); // Vinay
+  //   console.log('mockFn.mockImplementation(fn) testing 4 => ', myMock.mock.calls[0][0] === 'Vinay'); // true
+  //   console.log('mockFn.mockImplementation(fn) testing 5 => ', a); // hello Vinay
+  // });
+
+  // it('mockFn.mockImplementationOnce(fn) testing', () => {
+  //   var myMockFn = jest.fn()
+  //     .mockImplementationOnce(cb => cb(null, true))
+  //     .mockImplementationOnce(cb => cb(null, false));
+
+  //   myMockFn((err, val) => console.log(val));
+
+  //   myMockFn((err, val) => console.log(val));
+  // });
+
+  // it('mockFn.mockImplementationOnce(fn) testing 2', () => {
+  //   var myMockFn = jest.fn(() => 'default')
+  //     .mockImplementationOnce(() => 'first call')
+  //     .mockImplementationOnce(() => 'second call');
+
+  //   console.log(myMockFn(), myMockFn(), myMockFn(), myMockFn());
+  // });
+
+  // it('.mock property testing', () => {
+  //   const myMock = jest.fn();
+
+  //   const a = new myMock('first arg', 'Second arg');
+  //   const c = new myMock();
+  //   const b = {};
+  //   const bound = myMock.bind(b);
+  //   bound();
+
+  //   // console.log(myMock.mock);
+
+  //   // // The function was called exactly once
+  //   // expect(myMock.mock.calls.length).toBe(3);
+
+  //   // console.log('==== myMock.mock.calls', myMock.mock.calls[0]);
+  //   // // The first arg of the first call to the function was 'first arg'
+  //   // expect(myMock.mock.calls[0][0]).toBe('first arg');
+
+  //   console.log('==== myMock.mock.instances', myMock.mock.instances);
+  //   // This function was instantiated exactly twice
+  //   expect(myMock.mock.instances.length).toBe(3);
+  // });
+
+  it('Mock Return Values testing', () => {
+    const myMock = jest.fn();
+    console.log(myMock());
+    // > undefined
+
+    myMock.mockReturnValueOnce(10)
+    .mockReturnValueOnce('x')
+    .mockReturnValue(true);
+
+    console.log(myMock(), myMock(), myMock(), myMock());
+    // > 10, 'x', true, true
+  });
+
+  // it('myMock.mock.calls testing', () => {
+  //   const myMock = jest.fn((name) => {
+  //     return `hello ${name}`;
+  //   });
+  //   console.log('myMock.mock.calls testing 1 => ', myMock.mock.calls);
+  //   myMock('1');
+  //   console.log('myMock.mock.calls testing 2 => ', myMock.mock);
+  //   myMock('a', 'b');
+  //   console.log('myMock.mock.calls testing 3 => ', myMock());
+  //   console.log('myMock.mock.calls testing 4 => ', myMock('Vinay'));
+  //   // > [ [1], ['a', 'b'] ]
+  // });
+  // it('Test function mocking', () => {
+  //   const myMock = jest.fn();
+  //   const a = new myMock();
+  //   const b = {};
+  //   const bound = myMock.bind(b);
+  //   bound();
+  //   console.log('Module mocking', moduleName());
+  // });
+
+  // it('Test function mocking', () => {
+  //   const myMock = jest.fn();
+  //   const a = new myMock();
+  //   const b = {};
+  //   const bound = myMock.bind(b);
+  //   bound();
+  //   console.log('Function mocking', myMock.mock.calls);
+  // });
+
+  // it('Test function mocking', () => {
+  //   const myMock = jest.fn();
+  //   const a = new myMock();
+  //   const b = {};
+  //   const bound = myMock.bind(b);
+  //   bound();
+  //   console.log('Function mocking', myMock.mock.calls);
+  // });
+});
 
 /* Mocking */
 /* Stop=============================================================== */
--- __tests__/user-test.js
+++ __tests__/user-test.js
@@ -4,14 +4,14 @@ import * as user from '../test/user';
 jest.mock('../test/request');
 
 
-// // The assertion for a promise must be returned.
-// it('works with promises', () => {
-//   expect.assertions(1);
-//   return user.getUserName(4).then(data => {
-//     console.log('Receiving data', data);
-//     expect(data).toEqual('Vinay 4');
-//     });
-// });
+// The assertion for a promise must be returned.
+it('works with promises', () => {
+  expect.assertions(1);
+  return user.getUserName(4).then(data => {
+    console.log('Receiving data', data);
+    expect(data).toEqual('Vinay 4');
+    });
+});
 
 
 // it('works with resolves', () => {
@@ -19,16 +19,58 @@ jest.mock('../test/request');
 //   return expect(user.getUserName(5)).resolves.toEqual('Vinay 5');
 // });
 
-// async/await can be used.
-it('works with async/await', async () => {
-  expect.assertions(1);
-  const data = await user.getUserName(4);
-  console.log('Receiving data');
-  expect(data).toEqual('Vinay 4');
-});
+// /* async/await can be used. */
+// it('works with async/await', async () => {
+//   expect.assertions(1);
+//   const data = await user.getUserName(4);
+//   console.log('Receiving data');
+//   expect(data).toEqual('Vinay 4');
+// });
 
-// async/await can also be used with `.resolves`.
-it('works with async/await and resolves', async () => {
-  expect.assertions(1);
-  await expect(user.getUserName(5)).resolves.toEqual('Vinay 5');
-});
+// // async/await can also be used with `.resolves`.
+// it('works with async/await and resolves', async () => {
+//   expect.assertions(1);
+//   await expect(user.getUserName(5)).resolves.toEqual('Vinay 5');
+// });
+
+// /* Error handling */
+// // Testing for async errors using Promise.catch.
+// test('tests error with promises', async () => {
+//   expect.assertions(1);
+//   return user.getUserName(2).catch((e) => {
+//     console.log('receiving error ', e);
+//     expect(e).toEqual({
+//       error: 'User with 2 not found.',
+//     });
+//   });
+// });
+
+// // Or using async/await.
+// it('tests error with async/await', async () => {
+//   expect.assertions(1);
+//   try {
+//     await user.getUserName(1);
+//   } catch (e) {
+//     console.log('receiving error ', e);
+//     expect(e).toEqual({
+//       error: 'User with 1 not found.',
+//     });
+//   }
+// });
+
+// /* .rejects */
+// // Testing for async errors using `.rejects`.
+// it('tests error with rejects', () => {
+//   expect.assertions(1);
+//   return expect(user.getUserName(3)).rejects.toEqual({
+//     error: 'User with 3 not found.',
+//   });
+// });
+
+// // Or using async/await with `.rejects`.
+// it('tests error with async/await and rejects', async () => {
+//   expect.assertions(1);
+//   await expect(user.getUserName(3)).rejects.toEqual({
+//     error: 'User with 3 not found.',
+//   });
+// });
